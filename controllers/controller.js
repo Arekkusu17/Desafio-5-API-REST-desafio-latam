82a1a21e83b23b4639536a8ced9180a688e825fb
Arekkusu17 <fernn.alex@gmail.com>
2023-06-11 22:14:20 -0400

    add try catch statements

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,18 +1,31 @@
 import { myModel } from "../models/model.js";
-// import {getJewels} from "../"
+import { handleErrors } from "../database/errors.js";
 
 const getJewelsWithQueryParams = async (req, res) => {
-  const { limits, order_by, page } = req.query
-  const jewels = await myModel.getJewels({ limits, order_by, page })
-  const HATEOAS = myModel.prepareHATEOAS(jewels)
-  res.json(HATEOAS)
+  try {
+    const { limits, order_by, page } = req.query
+    const jewels = await myModel.getJewels({ limits, order_by, page })
+    const HATEOAS = myModel.prepareHATEOAS(jewels)
+    res.json(HATEOAS)
+  } catch (error) {
+    console.log(error);
+    const { status, message } = handleErrors(error.code);
+    return res.status(status).json({ ok: false, result: message });
+  }
+
 };
 
 const getJewelsWithFilters = async (req, res) => {
-  const queryString = req.query;
-  const jewels = await myModel.getJewelsWithFilters(queryString)
+  try {
+    const queryString = req.query;
+    const jewels = await myModel.getJewelsWithFilters(queryString)
+    res.json(jewels)
+  } catch (error) {
+    console.log(error)
+    const { status, message } = handleErrors(error.code);
+    return res.status(status).json({ ok: false, result: message });
+  }
 
-  res.json(jewels)
 
 }
 
